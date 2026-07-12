a242f7e76da9fb9d449f4d73d1dbd689366215de
Mailton Nascimento <mailton.st1@gmail.com>
2026-07-12 15:55:13 -0300

    fix: rola ate o carrinho no emulador CI

--- testes-mobile/src/test/java/br/com/outsera/mobile/tela/TelaBase.java
+++ testes-mobile/src/test/java/br/com/outsera/mobile/tela/TelaBase.java
@@ -1,6 +1,7 @@
 package br.com.outsera.mobile.tela;
 
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.AppiumBy;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -19,5 +20,6 @@ abstract class TelaBase {
 
     protected WebElement visivel(By locator) { return espera.until(ExpectedConditions.visibilityOfElementLocated(locator)); }
     protected void tocar(By locator) { espera.until(ExpectedConditions.elementToBeClickable(locator)).click(); }
+    protected void tocarAposRolar(String id) { driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().resourceId(\"" + id + "\"))")).click(); }
     protected void preencher(By locator, String valor) { WebElement campo = visivel(locator); campo.clear(); campo.sendKeys(valor); }
 }
